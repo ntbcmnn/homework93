4b37e1b6d301d189637110195ccdc4230fb695d9
Natalya Botsman <botsmannatashaa@gmail.com>
2025-02-17 13:41:36 +0600

    [HW-94] Removed a validation that checked if the user exists, because UseGuards already has this type of validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Post,
-  Req,
-  UnauthorizedException,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Post, Req, UseGuards } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from '../schemas/user.schema';
@@ -48,8 +40,6 @@ export class UsersController {
     if (user) {
       user.generateToken();
       await user.save();
-    } else {
-      throw new UnauthorizedException('User not found');
     }
 
     return { message: 'Successfully logged out' };
